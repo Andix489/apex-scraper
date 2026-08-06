7ac8394b3b3b11d5dbd4f2682d818392caef3d5c
Andix489 <sidneyandix@gmail.com>
2026-08-06 14:02:33 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -104,6 +104,11 @@ elif menu == "🔍 Live Scraper":
                 options.add_argument("--headless")
                 options.add_argument("--no-sandbox")
                 options.add_argument("--disable-dev-shm-usage")
+                options.add_argument("--disable-gpu")
+                options.add_argument("--window-size=1920,1080")
+                options.add_argument("--disable-blink-features=AutomationControlled")
+                options.add_experimental_option("excludeSwitches", ["enable-automation"])
+                options.add_experimental_option("useAutomationExtension", False)
                 options.binary_location = "/usr/bin/chromium"
 
                 service = webdriver.chrome.service.Service("/usr/bin/chromedriver")
